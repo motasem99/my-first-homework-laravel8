54e616a21c9cb8be7e8428db4caf30e88759c39e
mutasem <motasemimad10@gmail.com>
2021-02-25 15:16:45 +0200

    delete footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -370,14 +370,7 @@
         </div>
     </aside>
 
-    <footer class="footer">
-        <span class="text-left">
-            <a href="http://coreui.io">CoreUI</a> &copy; 2016 creativeLabs.
-        </span>
-        <span class="pull-right">
-            Powered by <a href="http://coreui.io">CoreUI</a>
-        </span>
-    </footer>
+
     <!-- Bootstrap and necessary plugins -->
     <script src="{{url('assets')}}/js/libs/jquery.min.js"></script>
     <script src="{{url('assets')}}/js/libs/tether.min.js"></script>
